0cc8bb95fc6a3ed1efe68b843f1b9efdb9a579bd
misha <mikhail.romanenko.in@gmail.com>
2016-02-08 12:42:35 +0200

    Initial commit

---
+++ src/main/java/com/timekeeping/TimekeepingApplication.java
@@ -0,0 +1,12 @@
+package com.timekeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimekeepingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimekeepingApplication.class, args);
+	}
+}
