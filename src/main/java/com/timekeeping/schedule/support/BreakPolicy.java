a59543b04de08ba5083c2d889d996e407227653d
misha <mikhail.romanenko.in@gmail.com>
2016-03-04 19:44:07 +0200

    Comments added.

--- src/main/java/com/timekeeping/schedule/support/BreakPolicy.java
+++ src/main/java/com/timekeeping/schedule/support/BreakPolicy.java
@@ -2,6 +2,21 @@ package com.timekeeping.schedule.support;
 
 import java.util.function.UnaryOperator;
 
+/**
+ * Abstraction representing a service capable of providing logic for calculation
+ * of the working time without break.
+ * 
+ * @author Mikhail Romanenko
+ *
+ */
 public interface BreakPolicy {
+
+	/**
+	 * Provides logic for mapping overall working type to working time without
+	 * break.
+	 * 
+	 * @return {@link UnaryOperator} implementation which apply mapping logic to
+	 *         {@code Integer} values.
+	 */
 	UnaryOperator<Integer> mapper();
 }
