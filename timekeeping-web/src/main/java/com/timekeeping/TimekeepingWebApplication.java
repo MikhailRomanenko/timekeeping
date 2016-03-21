e6daf663a0b503c80be22be4c074c801080bd68a
misha <mikhail.romanenko.in@gmail.com>
2016-03-21 17:09:23 +0200

    Add Web-project sub-module

---
+++ timekeeping-web/src/main/java/com/timekeeping/TimekeepingWebApplication.java
@@ -0,0 +1,12 @@
+package com.timekeeping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimekeepingWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimekeepingWebApplication.class, args);
+	}
+}
