7ba1b358facc11141b173fbadc63e9e6d98fad0d
misha <mikhail.romanenko.in@gmail.com>
2016-04-05 13:03:35 +0300

    Create login page template

--- timekeeping-web/src/main/java/com/timekeeping/TimekeepingWebApplication.java
+++ timekeeping-web/src/main/java/com/timekeeping/TimekeepingWebApplication.java
@@ -2,11 +2,27 @@ package com.timekeeping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class TimekeepingWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TimekeepingWebApplication.class, args);
+	}
+	
+	@Profile("dev")
+	@Configuration
+	static class MvcConfig extends WebMvcConfigurerAdapter {
+
+		@Override
+		public void addViewControllers(ViewControllerRegistry registry) {
+			registry.addViewController("/login").setViewName("login");
+			super.addViewControllers(registry);
+		}
+		
 	}
 }
