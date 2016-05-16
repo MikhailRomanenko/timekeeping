c115f17cf9f4c11133f051714efd0b6c68d29233
misha <mikhail.romanenko.in@gmail.com>
2016-05-16 18:31:14 +0300

    Allow EmployeeBox directive to remove items on select.
    On item selecting it is removed and callback 'select' is called with removed item.

--- timekeeping-client/src/main/js/employeeBox.js
+++ timekeeping-client/src/main/js/employeeBox.js
@@ -7,8 +7,25 @@ angular.module('employee-box')
             replace: true,
             scope: {
                 departments: "=employees",
-                select: "&"
+                onSelect: "&select"
             },
-            templateUrl: '/templates/employee-box.html'
+            controller: ['$scope', function($scope){
+                 $scope.select = function(department, employee) {
+                     var iDept = $scope.departments.findIndex(function(elem){
+                         if(department === elem.departmentName) return true;
+                         return false;
+                     });
+                     var iRemove = $scope.departments[iDept].employees.findIndex(function(elem){
+                         if(elem.id === employee.id) return true;
+                         return false;
+                     });
+                     $scope.departments[iDept].employees.splice(iRemove, 1);
+                     if($scope.departments[iDept].employees.length === 0) 
+                         $scope.departments.splice(iDept, 1);
+                     if(angular.isFunction($scope.onSelect))
+                         $scope.onSelect({employee: employee});
+                 }
+            }],
+            templateUrl: '../templates/employee-box.html'
         };
     });
